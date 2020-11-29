70953916450e61833fdab3c757bb2c110b52108f
ArlearD <azelird@gmail.com>
2020-11-29 14:00:22 +0300

    Binary save

--- Archivator/Archivator/Program.cs
+++ Archivator/Archivator/Program.cs
@@ -9,8 +9,6 @@ namespace Archivator
 {
     class Program
     {
-        static Dictionary<string, int> dict = new Dictionary<string, int>();
-        static Dictionary<int, string> decodeDict = new Dictionary<int, string>();
         static void Main(string[] args)
         {
             var location = Assembly
@@ -28,69 +26,87 @@ namespace Archivator
             stopWatch.Start();
             DeCompress(location + "archived.txt", location + "deCompressed.txt");
             stopWatch.Stop();
-
             System.Console.WriteLine(stopWatch.ElapsedMilliseconds);
-
         }
 
         public static void Compress(string fileToCompressPath, string compressedFilePath)
         {
-            //var dict = new Dictionary<string, int>();
-            //var decodeDict = new Dictionary<int, string>();
+            var dict = new Dictionary<string, int>();
 
             var inputString = File
                 .OpenText(fileToCompressPath)
                 .ReadToEnd();
 
+            StringBuilder dictionary = new StringBuilder();
 
             foreach (var value in inputString)
             {
                 var currentStringValue = value.ToString();
                 if (!dict.ContainsKey(currentStringValue))
                 {
+                    dictionary.Append(currentStringValue);
                     dict[currentStringValue] = dict.Count;
-                    decodeDict[decodeDict.Count] = currentStringValue;
                 }
             }
 
-            string s = "";
+            var key = new StringBuilder();
             char buffer;
-            StringBuilder output = new StringBuilder();
+
+            var outputValues = new List<int>();
 
             foreach (var inputChar in inputString)
             {
                 buffer = inputChar;
-                if (dict.ContainsKey(s + buffer))
+                if (dict.ContainsKey(key.ToString() + buffer))
                 {
-                    s = s + buffer;
+                    key.Append(buffer);
                 }
                 else
                 {
-                    output.Append(dict[s] + "\n");
-                    dict[s + buffer] = dict.Count;
-                    s = buffer.ToString();
+                    outputValues.Add(dict[key.ToString()]);
+                    dict[key.ToString() + buffer] = dict.Count;
+                    key = new StringBuilder(buffer.ToString());
                 }
             }
 
-            output.Append(dict[s]);
+            outputValues.Add(dict[key.ToString()]);
 
-            File.WriteAllText(compressedFilePath, output.ToString());
+            using (FileStream fs = new FileStream(compressedFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+            using (BinaryWriter binWriter = new BinaryWriter(fs))
+            {
+                binWriter.Write(dictionary.ToString());
+                binWriter.Write(outputValues.Count);
+                foreach (var compressedValue in outputValues)
+                    binWriter.Write(compressedValue);
+            }
         }
 
         public static void DeCompress(string fileToDeCompressPath, string deCompressedFilePath)
         {
+            var decodeDict = new Dictionary<int, string>();
             StringBuilder decodeResult = new StringBuilder();
+            int[] decodeCodes;
 
-            var decodeCodes = File.ReadAllLines(fileToDeCompressPath)
-                .Select(x => int.Parse(x))
-                .ToList();
+            using (FileStream fs = new FileStream(fileToDeCompressPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (BinaryReader binReader = new BinaryReader(fs))
+            {
+                foreach (var value in binReader.ReadString())
+                    decodeDict[decodeDict.Count] = value.ToString();
+
+                int quantity = binReader.ReadInt32();
+
+                decodeCodes = new int[quantity];
+
+                for (int i = 0; i < quantity; i++)
+                    decodeCodes[i] = binReader.ReadInt32();
+            }
 
             var previousCode = decodeCodes[0];
             var firstPhrase = decodeDict[previousCode];
 
             decodeResult.Append(firstPhrase);
 
-            for (int i = 1; i < decodeCodes.Count; i++)
+            for (int i = 1; i < decodeCodes.Length; i++)
             {
                 var currentCode = decodeCodes[i];
 
