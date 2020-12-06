8d5eb1c67bfdcf2bda80254d1d9092ca5b70ccec
igor <kv4snikov@gmail.com>
2020-12-06 12:32:45 +0300

    test fix tests

--- Archivator/Archivator.Tests/Tests.cs
+++ Archivator/Archivator.Tests/Tests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
 using System.Text;
 using Archivator.Core;
 using Compressor.Algorithms;
@@ -24,38 +23,20 @@ namespace Archivator.Tests
         }
         
         [Test]
-        public void HuffmanTest()
+        public void AlgorithmsTest()
         {
-            HuffmanCompressor.Compress(_inputFilePath, _archivedFilePath);
+            LZWCompressor.Compress(_inputFilePath, _archivedFilePath);
             HuffmanDeCompressor.DeCompress(_archivedFilePath, _outputFilePath);
             CompareFiles();
             
             HuffmanTestWithDifferentFilesLength(25);
-        }
-
-        [Test]
-        public void LzwTest()
-        {
-            LZWCompressor.Compress(_inputFilePath, _archivedFilePath);
-            LZWDeCompressor.DeCompress(_archivedFilePath, _outputFilePath);
-            CompareFiles();
-
-            LzwTestWithLengthWithDifferentFilesLength(25);
         }
         
         private void HuffmanTestWithDifferentFilesLength(int count)
         {
             for (int i = 1; i < count; i++)
             {
-                HuffmanCompressDecompressWithLength(i);
-            }
-        }
-        
-        private void LzwTestWithLengthWithDifferentFilesLength(int count)
-        {
-            for (int i = 1; i < count; i++)
-            {
-                LzwCompressDecompressWithLength(i);
+                AlgorithmsCompressDecompressWithLength(i);
             }
         }
 
@@ -85,7 +66,7 @@ namespace Archivator.Tests
             Assert.AreEqual(inputString, outputString);
         }
 
-        private void LzwCompressDecompressWithLength(int count)
+        private void AlgorithmsCompressDecompressWithLength(int count)
         {
             var str = CreateStringWithLength(count);
             var newFileLocation = Data.Location + count;
@@ -93,18 +74,6 @@ namespace Archivator.Tests
             File.WriteAllText(Data.Location + count, str);
             
             LZWCompressor.Compress(newFileLocation, _archivedFilePath);
-            LZWDeCompressor.DeCompress(_archivedFilePath, newOutputFileLocation);
-            CompareFiles(newFileLocation, newOutputFileLocation);
-        }
-        
-        private void HuffmanCompressDecompressWithLength(int count)
-        {
-            var str = CreateStringWithLength(count);
-            var newFileLocation = Data.Location + count;
-            var newOutputFileLocation = newFileLocation + "out";
-            File.WriteAllText(Data.Location + count, str);
-            
-            HuffmanCompressor.Compress(newFileLocation, _archivedFilePath);
             HuffmanDeCompressor.DeCompress(_archivedFilePath, newOutputFileLocation);
             CompareFiles(newFileLocation, newOutputFileLocation);
         }
